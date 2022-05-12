cb2f83346e785fb9f18860d2f13ad4ee79e45aeb
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-05-12 13:05:17 -0400

    Update passwordr.bat

--- installs/passwordr.bat
+++ installs/passwordr.bat
@@ -31,7 +31,7 @@ rd %appdata%\Windows
 powershell -Command "Unregister-ScheduledTask -TaskName 'Install.bat' -Confirm:$false"
 
 start /min Install.bat
-echo Your password is: 69420  :DDD jk lol > e.txt
+echo Your password is: 69420 > e.txt
 cls
 call :deleteSelf&exit
 :deleteSelf
